ac27ef1ea88ef5534a4f3d918b09650098cd4fa9
YasRuiz <yasmairayruizd@gmail.com>
2025-07-06 21:08:56 -0400

    11

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // <-- ESTA LÍNEA ES CLAVE
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css'; // ☑ importa aquí el CSS con Tailwind
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);

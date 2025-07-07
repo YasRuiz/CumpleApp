43e9a85b7f31c34aa5ad22b17150bc030c8ebc4c
YasRuiz <yasmairayruizd@gmail.com>
2025-07-06 21:06:31 -0400

    10

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // <-- este es el CSS con Tailwind
+import './index.css' // <-- ESTA LÍNEA ES CLAVE
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

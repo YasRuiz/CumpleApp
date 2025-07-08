de712ec0001e31bf267b097ef5e2080d5ef1c515
YasRuiz <yasmairayruizd@gmail.com>
2025-07-07 21:07:12 -0400

    Actualizar vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/CumpleApp/',
+  base: '/',
   plugins: [
     react(),
     VitePWA({
@@ -32,5 +32,3 @@ export default defineConfig({
     })
   ]
 });
-
-
